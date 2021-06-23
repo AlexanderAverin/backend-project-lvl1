338298de243203dee5a6269aa85aa3694f8a0192
Alexander Averin <averin.alexander@outlook.com>
2021-06-23 18:44:28 +0300

    Renamed several entities

--- src/games/brain-calcLogic.js
+++ src/games/brain-calcLogic.js
@@ -1,26 +1,26 @@
 import CreateGame from '../index.js';
 
 const GiveRandomExp = (FirstNum, SecondNum) => {
-  const operations = [['+', FirstNum + SecondNum], ['-', FirstNum - SecondNum], ['*', FirstNum * SecondNum]];
-  const element = operations[Math.floor(Math.random() * 3)];
-  return [`${FirstNum} ${element[0]} ${SecondNum}`, element[1]];
+  const Operations = [['+', FirstNum + SecondNum], ['-', FirstNum - SecondNum], ['*', FirstNum * SecondNum]];
+  const RandomOperation = Operations[Math.floor(Math.random() * 3)];
+  return [`${FirstNum} ${RandomOperation[0]} ${SecondNum}`, RandomOperation[1]];
 };
 
 const CreateBrainCalc = () => {
   const rules = 'What is the result of the expression?';
-  const questions = [];
-  const correctRepls = [];
+  const Questions = [];
+  const CorrectRepls = [];
 
   let i = 0;
   let Expression = null;
 
   while (i < 3) {
     Expression = GiveRandomExp(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100));
-    questions.push(Expression[0]);
-    correctRepls.push(String(Expression[1]));
+    Questions.push(Expression[0]);
+    CorrectRepls.push(String(Expression[1]));
     i += 1;
   }
-  return CreateGame(questions, correctRepls, rules);
+  return CreateGame(Questions, CorrectRepls, rules);
 };
 
 export default CreateBrainCalc;
